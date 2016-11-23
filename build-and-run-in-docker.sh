6184f5e0cef57d7e918decc620d6593e9e9f9c2f
Alexander Fefelov <alexanderfefelov@yandex.ru>
2016-11-23 16:50:58 +0300

    Containerize the app using Docker

---
+++ build-and-run-in-docker.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+HOME=`dirname $0`
+cd $HOME
+
+bin/activator playUpdateSecret
+bin/activator dist
+
+docker build --tag alexanderfefelov/area-451 .
+docker run --name area-451 --detach --tty \
+  --publish 80:9000 \
+  --volume /etc/localtime:/etc/localtime:ro \
+  --volume /etc/timezone:/etc/timezone:ro \
+  alexanderfefelov/area-451
