e9ce606d25688be484f355aed2a35b513446c036
Alexander Fefelov <alexanderfefelov@yandex.ru>
2016-12-05 09:56:47 +0300

    Implement access logging

---
+++ app/filters/AccessLoggingFilter.scala
@@ -0,0 +1,29 @@
+package filters
+
+import javax.inject.Inject
+
+import akka.stream.Materializer
+import play.api.Logger
+import play.api.mvc._
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
+
+class AccessLoggingFilter @Inject() (implicit val mat: Materializer) extends Filter {
+
+  val accessLogger = Logger("access")
+
+  def apply(next: (RequestHeader) => Future[Result])(request: RequestHeader): Future[Result] = {
+    val resultFuture = next(request)
+
+    resultFuture.foreach(result => {
+      val message = s"request-id=${request.id} remote-address=${request.remoteAddress}" +
+        s" user-agent=${request.headers.get("User-Agent").getOrElse("")}" +
+        s" host=${request.host} uri=${request.uri} method=${request.method}" +
+        s" status=${result.header.status}"
+      accessLogger.info(message)
+    })
+
+    resultFuture
+  }
+}
--- app/filters/Filters.scala
+++ app/filters/Filters.scala
@@ -6,6 +6,6 @@ import com.mohiva.play.htmlcompressor.HTMLCompressorFilter
 import play.api.http.DefaultHttpFilters
 import play.filters.gzip.GzipFilter
 
-class Filters @Inject() (gzipFilter: GzipFilter, htmlCompressorFilter: HTMLCompressorFilter) extends DefaultHttpFilters(
-  gzipFilter, htmlCompressorFilter
+class Filters @Inject() (accessLoggingFilter: AccessLoggingFilter, gzipFilter: GzipFilter, htmlCompressorFilter: HTMLCompressorFilter) extends DefaultHttpFilters(
+  accessLoggingFilter, gzipFilter, htmlCompressorFilter
 )
