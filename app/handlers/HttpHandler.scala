176e8c7d885367f40e42fcb7b78028dd1c7c0253
Alexander Fefelov <alexanderfefelov@yandex.ru>
2016-12-27 04:23:16 +0300

    Handle HTTP requests with empty path element

---
+++ app/handlers/HttpHandler.scala
@@ -0,0 +1,20 @@
+package handlers
+
+import javax.inject.Inject
+
+import play.api.http._
+import play.api.mvc.{Handler, RequestHeader}
+import play.api.routing.Router
+
+class HttpHandler @Inject()(router: Router, errorHandler: HttpErrorHandler, configuration: HttpConfiguration, filters: HttpFilters)
+  extends DefaultHttpRequestHandler(router, errorHandler, configuration, filters) {
+
+  override def routeRequest(requestHeader: RequestHeader): Option[Handler] = {
+    val normalizedRequestHeader = requestHeader.path match {
+      case "" => requestHeader.copy(path = "/", uri = "/" + requestHeader.uri)
+      case _ => requestHeader
+    }
+    super.routeRequest(normalizedRequestHeader)
+  }
+
+}
